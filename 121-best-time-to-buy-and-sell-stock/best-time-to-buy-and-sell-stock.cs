e99d0be26fddc1d929408c3edaad78c0dedeb879
SIDDHARTH SINGH TIWARI <75925726+Siddharth2-sidd@users.noreply.github.com>
2026-07-03 22:49:21 +0530

    Time: 1 ms (99.87%) | Memory: 56.1 MB (81.63%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int profit = 0;
+        int buy = int.MaxValue;
+        for(int i=0; i<prices.Length; i++){
+            if(prices[i] < buy){
+                buy = prices[i];
+            }else if((prices[i]-buy) > profit){
+                profit = prices[i] - buy;
+            }
+        }
+
+    return profit;
+    }
+}
