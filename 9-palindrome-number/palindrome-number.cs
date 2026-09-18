abb3360f058a739db61ec2e1b85e7572dd6964d5
SIDDHARTH SINGH TIWARI <75925726+Siddharth2-sidd@users.noreply.github.com>
2026-09-19 01:00:41 +0530

    Time: 1 ms (100.00%) | Memory: 36.7 MB (81.99%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        int rev = reverse(x);
+        if(x<0 || x != rev){
+            return false;
+        }
+        return true;
+    }
+
+    public int reverse(int x){
+        int rev = 0;
+        while(x!=0){
+            int rem = x%10;
+            rev = rev*10+rem;
+            x = x/10;
+        }
+        return rev;
+    }
+}
